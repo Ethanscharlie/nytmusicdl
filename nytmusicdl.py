880dca60a95d725486cd7d1a277742f06a4f39b9
Ethanscharlie <ethanscharlie@protonmail.com>
2024-12-27 23:03:54 -0700

    Removed unnessessary oop and put most of code in one function

--- nytmusicdl.py
+++ nytmusicdl.py
@@ -27,168 +27,133 @@ VIDEO_FILE_EXT = "mp4"
 AUDIO_FILE_EXT = "mp3"
 PLAYLIST_FILTER = "&sp=EgIQAw%253D%253D"
 
-class AlbumInfo:
-    def __init__(self, album: str, artist: str, cover_art_url: str, tracklist: [str]):
-        self.album = album
-        self.artist = artist
-        self.tracklist = tracklist
-        self.cover_art_url = cover_art_url
-        self.yt_urls = [str]
 
-    def download(self, folder: path):
-        download_start_time = time.time()
+def download(
+    album: str, artist: str, cover_art_url: str, tracklist: list[str], folder: str
+):
+    download_start_time = time.time()
 
-        self.yt_urls = self.search_album(f"{self.album} {self.artist}")
+    # Find a playlist and get the list of video urls
+    yt_urls: list[str] = []
+    query = f"{album} {artist}"
+    search_url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}{PLAYLIST_FILTER}"
+    response = requests.get(search_url)
 
-        self.cover_path = self._AlbumInfo__get_cover_art(self.cover_art_url, folder)
+    pattern = r'playlist\?list=(.*?)"'
+    playlist_links = re.findall(pattern, response.text)
 
-        print("\n")
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for index, track_name in enumerate(self.tracklist):
-                executor.submit(self.download_and_assign_metadata_to_song, folder, track_name, index)
+    if not playlist_links:
+        raise Exception(f"Nothing not found at search url {playlist_links}")
 
-        download_time = time.time() - download_start_time
-        print(f"Download took {download_time:.2f} seconds")
+    print(playlist_links)
 
-    def download_and_assign_metadata_to_song(self, folder: path, track_name: str, index: int):
-        track_url = self.yt_urls[index]
-        print(f"Name: {track_name}, Url: {track_url}, index: {index}")
-        self._AlbumInfo__download_video(track_url, track_name, folder)
-        self._AlbumInfo__assign_metadata_to_file(path.join(folder, f"{track_name}.mp3"), track_name, index+1)
+    for id in playlist_links:
+        playlist_url = f"https://www.youtube.com/playlist?list={id}"
+        print(playlist_url)
 
-    def __assign_metadata_to_file(self, file: str, title: str, index: int=1):
-        ''' Sets the metadata (album name, title, art, etc.) for the mp3 file '''
-    
-        audio = EasyID3(file)
-    
-        audio.delete()
-        audio['title'] = title
-        audio["album"] = self.album    
-        audio['artist'] = self.artist
-        audio['tracknumber'] = str(index)
-        audio.save()
-    
-        if self.cover_path:
-            self._AlbumInfo__set_cover_art(file, self.cover_path)
-    
-        return audio
-
-    def search_song(self, query):
-        search_url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
-        response = requests.get(search_url)
-    
-        # Regular expression to find video links
-        pattern = r'watch\?v=(.*?)"'
-        video_links = re.findall(pattern, response.text)
-    
-        if not video_links:
-            raise Exception(f"Video not found at url {search_url}")
-            
-        return f"https://www.youtube.com/watch?v={video_links[0][:-1]}"
-
-    def get_videos_from_playlist(self, playlist_url: str) -> [str]:
         response = requests.get(playlist_url)
-    
-        with open("test.html", "w+") as f:
-            f.write(response.text)
-    
-        start_index = response.text.index(r'')
+
+        start_index = response.text.index(r"")
         end_index = response.text.index(r"innertubeCommand")
-        list_text = response.text[start_index : end_index]
-    
-        video_links = []
+        list_text = response.text[start_index:end_index]
+
+        yt_urls = []
         video_pattern = r'watch\?v=(.*?)"'
         video_ids = re.findall(video_pattern, list_text)
-    
+
         if not video_ids:
             raise Exception(f"No watch links found on playlist page")
-    
+
         for id in video_ids:
             # Filter out
             AndIndex = id.find("\\u0026")
-            if AndIndex == -1: continue
+            if AndIndex == -1:
+                continue
             id = id[:AndIndex]
-    
+
             video_url = f"https://www.youtube.com/watch?v={id}"
             print(video_url)
-            video_links.append(video_url)
-            
-        return video_links
-    
-    
-    def search_album(self, query):
-        search_url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}{PLAYLIST_FILTER}"
-        response = requests.get(search_url)
-        
-        pattern = r'playlist\?list=(.*?)"'
-        playlist_links = re.findall(pattern, response.text)
-    
-        if not playlist_links:
-            raise Exception(f"Nothing not found at search url {playlist_links}")
+            yt_urls.append(video_url)
 
-        print(playlist_links)
-    
-        for id in playlist_links:
-            playlist_url = f"https://www.youtube.com/playlist?list={id}"
-            print(playlist_url)
-            video_urls = self.get_videos_from_playlist(playlist_url)
-    
-            if (len(video_urls) > len(self.tracklist)):
-                print("Playlist is too long")
-                continue
+        if len(yt_urls) > len(tracklist):
+            print(f"Playlist is too long, yt_urls: {len(yt_urls)}, tracklist: {len(tracklist)}")
+            yt_urls = []
+            continue
+        else:
+            break
 
-            return video_urls
-
+    if len(yt_urls) == 0:
         raise Exception("No good playlists found")
 
-    def __set_cover_art(self, audio_file_location: str, image_location: str):
-        # Sets the album cover art of the file to the image
-    
-        audio = MP3(audio_file_location, ID3=ID3)
-    
-        audio.tags.add(APIC(
-            mime='image/jpeg',
-            type=3,
-            desc=u'Cover',
-            data=open(image_location, 'rb').read()
-        ))
-    
-        audio.save()
-    
-    def __get_cover_art(self, url: str, directory: str) -> path:
-        # Grabs and downloads the cover art after figuring out if it's a location, or a web image
-    
-        target_location = os.path.join(directory, 'cover.jpg')
-    
-        if not requests.get(url).status_code:
-            raise Exception("Image not found :(")
-    
-        img_file = wget.download(url, directory)
-        image = Image.open(img_file)
-    
-        # Save
-        image.save(target_location)
-        os.remove(img_file)
-    
-        return target_location
+    # Download the album cover art
+    target_location = os.path.join(folder, "cover.jpg")
 
-    def __download_video(self, url: str, filename: str, folder: path) -> None:
-        current_directory = os.getcwd()
-        os.system(f"cd '{folder}'")
+    if not requests.get(cover_art_url).status_code:
+        raise Exception("Image not found :(")
 
-        if str(filename) != "<class 'str'>":
-            command = f'yt-dlp -x --audio-format mp3 -o "{folder}/{filename}.%(ext)s" {url}'
-            print(command)
-            os.system(command)
+    img_file = wget.download(cover_art_url, folder)
+    image = Image.open(img_file)
 
-        os.system(f"cd '{current_directory}'")
+    # Save
+    image.save(target_location)
+    os.remove(img_file)
+
+    cover_path = target_location
+
+    print("\n")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for index, track_name in enumerate(tracklist):
+            def download_and_assign_metadata_to_song(index: int, track_name: str):
+                track_url = yt_urls[index]
+                print(f"Name: {track_name}, Url: {track_url}, index: {index}")
+
+                # Download video
+                current_directory = os.getcwd()
+                os.system(f"cd '{folder}'")
+
+                command = f'yt-dlp -x --audio-format mp3 -o "{folder}/{track_name}.%(ext)s" {track_url}'
+                print(command)
+                os.system(command)
+                os.system(f"cd '{current_directory}'")
+
+                # Assign metadata
+                audio_file = path.join(folder, f"{track_name}.mp3")
+                tracknumer = index + 1
+
+                audio = EasyID3(audio_file)
+
+                audio.delete()
+                audio["title"] = track_name
+                audio["album"] = album
+                audio["artist"] = artist
+                audio["tracknumber"] = str(tracknumer)
+                audio.save()
+
+                # Set Cover art
+                if cover_path:
+                    id3audio = MP3(audio_file, ID3=ID3)
+                    id3audio.tags.add(
+                        APIC(
+                            mime="image/jpeg",
+                            type=3,
+                            desc="Cover",
+                            data=open(cover_path, "rb").read(),
+                        )
+                    )
+                    id3audio.save()
+
+            executor.submit(download_and_assign_metadata_to_song, index, track_name)
+
+    download_time = time.time() - download_start_time
+    print(f"Download took {download_time:.2f} seconds")
+
 
 def get_tracklist(url: str):
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(responses.status_code)
-    
+        raise Exception(response.status_code)
+
     tracklist = []
     for track in response.json()["data"]:
         trackname = track["title"]
@@ -206,25 +171,29 @@ def get_tracklist(url: str):
     print(tracklist)
     return tracklist
 
-def search_music(search_term: str) -> [AlbumInfo]:
+
+def search_music(search_term: str) -> list[tuple[str, str, str, list[str]]]:
     url = f"https://api.deezer.com/search?q={search_term}"
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(responses.status_code)
+        raise Exception(response.status_code)
 
     data = response.json()
     responses = []
 
-    item = data.get('data', [])[0]
-    album_name = item.get('album', {}).get('title', 'N/A')
-    artist_name = item.get('artist', {}).get('name', 'N/A')
-    cover_art = item.get('album', {}).get('cover_big', 'N/A')  # Use cover_big for higher quality
+    item = data.get("data", [])[0]
+    album_name = item.get("album", {}).get("title", "N/A")
+    artist_name = item.get("artist", {}).get("name", "N/A")
+    cover_art = item.get("album", {}).get(
+        "cover_big", "N/A"
+    )  # Use cover_big for higher quality
 
     tracklist = get_tracklist(item["album"]["tracklist"])
-    responses.append(AlbumInfo(album_name, artist_name, cover_art, tracklist))
+    responses.append((album_name, artist_name, cover_art, tracklist))
 
     return responses
+
 
 def general_filter(text: str) -> str:
     text = text.replace("/", "-")
@@ -232,8 +201,8 @@ def general_filter(text: str) -> str:
     text = text.replace("&", "and")
     text = text.replace(":", " -")
     text = text.replace(",", " -")
-    text = text.replace("\"", " ")
-    text = text.replace("\'", " ")
+    text = text.replace('"', " ")
+    text = text.replace("'", " ")
     text = text.replace("?", " ")
     text = text.replace("*", " ")
     text = text.replace("<", "(")
@@ -241,59 +210,58 @@ def general_filter(text: str) -> str:
     text = text.replace("|", "-")
     return text
 
+
 def main():
-    if (sys.argv[1] == "-a"):
+    if sys.argv[1] == "-a":
         artist_name_search = sys.argv[2]
         url = f"https://api.deezer.com/search/artist?q={artist_name_search}"
         response = requests.get(url)
-        artist_name = general_filter(response.json().get('data', [])[0]["name"])
-        artist_id = response.json().get('data', [])[0]["id"]
+        artist_name = general_filter(response.json().get("data", [])[0]["name"])
+        print(f"Artist Name: {artist_name}")
+        artist_id = response.json().get("data", [])[0]["id"]
 
         url = f"https://api.deezer.com/artist/{artist_id}/albums?limit=1000"
         response = requests.get(url)
 
         if response.status_code != 200:
-            raise Exception(responses.status_code)
+            raise Exception(response.status_code)
 
         data = response.json()
 
-        for album_data in data.get('data', []):
+        for album_data in data.get("data", []):
             album_name = general_filter(album_data["title"])
             cover_art = album_data["cover_big"]
             tracklist = get_tracklist(album_data["tracklist"])
 
-            album_info = AlbumInfo(album_name, artist_name, cover_art, tracklist)
+            directory = sys.argv[3]
 
-            directory = sys.argv[2]
-
-            artist_path = path.join(directory, album_info.artist)
+            artist_path = path.join(directory, artist_name)
             if not path.isdir(artist_path):
                 os.mkdir(artist_path)
 
-            album_path = path.join(artist_path, album_info.album)
+            album_path = path.join(artist_path, album_name)
             if not path.isdir(album_path):
                 os.mkdir(album_path)
 
-
             try:
-                album_info.download(album_path)
+                download(album_name, artist_name, cover_art, tracklist, album_path)
             except Exception:
                 print("WARNING A PLAYLIST COULD NOT BE FOUND")
 
     else:
-        album_info = search_music(sys.argv[1])[0]
+        album_name, artist_name, cover_art, tracklist = search_music(sys.argv[1])[0]
 
         directory = sys.argv[2]
 
-        artist_path = path.join(directory, album_info.artist)
+        artist_path = path.join(directory, artist_name)
         if not path.isdir(artist_path):
             os.mkdir(artist_path)
 
-        album_path = path.join(artist_path, album_info.album)
+        album_path = path.join(artist_path, album_name)
         if not path.isdir(album_path):
             os.mkdir(album_path)
 
+        download(artist_name, album_name, cover_art, tracklist, album_path)
 
-        album_info.download(album_path)
 
 main()
